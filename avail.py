78643d8796a71008c7ca514983164fe4113c6038
bengzz <benjagzz@gmail.com>
2015-10-21 19:00:28 -0500

    Cubo semántico

---
+++ avail.py
@@ -0,0 +1,136 @@
+class avail:
+        cubo_semantico = None
+
+        def _init_(self):
+                self.cubo_semantico = {
+                '=': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'error',
+                                    'flotante': 'flotante'
+                        }
+                },
+                '<': {
+                        'entero': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        },
+                        'flotante': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        }
+                },
+                '>': {
+                        'entero': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        },
+                        'flotante': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        }
+                },
+                '<=': {
+                        'entero': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        },
+                        'flotante': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        }
+                },
+                '>=': {
+                        'entero': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        },
+                        'flotante': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        }
+                },
+                '<>': {
+                        'entero': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        },
+                        'flotante': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        }
+                },
+                '==': {
+                        'entero': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        },
+                        'flotante': {
+                                    'entero': 'booleano',
+                                    'flotante': 'booleano'
+                        }
+                },
+                '*': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'flotante',
+                                    'flotante': 'flotante'
+                        }
+                },
+                '/': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'flotante',
+                                    'flotante': 'flotante'
+                        }
+                }
+                '-': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'flotante',
+                                    'flotante': 'flotante'
+                        }
+                },
+                '+': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'flotante',
+                                    'flotante': 'flotante'
+                        }
+                },
+                '$': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'flotante',
+                                    'flotante': 'flotante'
+                        }
+                },
+                '%': {
+                        'entero': {
+                                    'entero': 'entero',
+                                    'flotante': 'flotante'
+                        },
+                        'flotante': {
+                                    'entero': 'flotante',
+                                    'flotante': 'flotante'
+                        }
+                }
+        }
